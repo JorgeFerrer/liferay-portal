d352eb31853253e25e2f523e6d46f568e9412672
Juan Fernández <juan.fernandez@liferay.com>
2011-07-11 18:17:50 +0200

    LPS-16829 Autogenerated

--- portal-service/src/com/liferay/portlet/journal/service/JournalStructureLocalService.java
+++ portal-service/src/com/liferay/portlet/journal/service/JournalStructureLocalService.java
@@ -242,7 +242,8 @@ public interface JournalStructureLocalService {
 	public com.liferay.portlet.journal.model.JournalStructure addStructure(
 		long userId, long groupId, java.lang.String structureId,
 		boolean autoStructureId, java.lang.String parentStructureId,
-		java.lang.String name, java.lang.String description,
+		java.util.Map<java.util.Locale, java.lang.String> nameMap,
+		java.util.Map<java.util.Locale, java.lang.String> descriptionMap,
 		java.lang.String xsd,
 		com.liferay.portal.service.ServiceContext serviceContext)
 		throws com.liferay.portal.kernel.exception.PortalException,
@@ -352,8 +353,10 @@ public interface JournalStructureLocalService {
 
 	public com.liferay.portlet.journal.model.JournalStructure updateStructure(
 		long groupId, java.lang.String structureId,
-		java.lang.String parentStructureId, java.lang.String name,
-		java.lang.String description, java.lang.String xsd,
+		java.lang.String parentStructureId,
+		java.util.Map<java.util.Locale, java.lang.String> nameMap,
+		java.util.Map<java.util.Locale, java.lang.String> descriptionMap,
+		java.lang.String xsd,
 		com.liferay.portal.service.ServiceContext serviceContext)
 		throws com.liferay.portal.kernel.exception.PortalException,
 			com.liferay.portal.kernel.exception.SystemException;
