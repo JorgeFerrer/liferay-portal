98a5ecb894b1f4cb9be5c382f919e86668c5e756
Jorge Ferrer <jorge.ferrer@liferay.com>
2012-11-23 10:38:50 +0100

    Revert "LPS-31036 Source formatting"
    This reverts commit 5ab896c1c94e0b58505aad75c09ef90e7224d87b.

--- portal-impl/src/com/liferay/portal/service/permission/PortletPermissionImpl.java
+++ portal-impl/src/com/liferay/portal/service/permission/PortletPermissionImpl.java
@@ -428,6 +428,7 @@ public class PortletPermissionImpl implements PortletPermission {
 			if ((groupId > 0) && layout.isTypeControlPanel() &&
 				Validator.equals(portletId, PortletKeys.SITES_ADMIN)) {
 
+				//special case to handle sites admin in control panel
 				group = GroupLocalServiceUtil.getGroup(groupId);
 			}
 			else {
