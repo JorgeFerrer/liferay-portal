0f78933524120a4d61d457862e9cc99d7746d149
Minhchau Dang <minhchau.dang@liferay.com>
2014-05-22 18:34:28 -0700

    LPS-47130 Deprecate no-arg constructors for exceptions in UserLocalService

--- portal-service/src/com/liferay/portal/ReservedUserEmailAddressException.java
+++ portal-service/src/com/liferay/portal/ReservedUserEmailAddressException.java
@@ -21,6 +21,7 @@ import com.liferay.portal.kernel.exception.PortalException;
  */
 public class ReservedUserEmailAddressException extends PortalException {
 
+	@Deprecated
 	public ReservedUserEmailAddressException() {
 		super();
 	}
