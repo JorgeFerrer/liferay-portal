209253b9fb05892d3860b527fd372b200c261b02
Pei-Jung Lan <pei-jung.lan@liferay.com>
2015-03-16 16:26:46 -0700

    LPS-53409 Provide explanation as to why validation fails

--- portal-service/src/com/liferay/portal/security/auth/DefaultScreenNameValidator.java
+++ portal-service/src/com/liferay/portal/security/auth/DefaultScreenNameValidator.java
@@ -14,6 +14,9 @@
 
 package com.liferay.portal.security.auth;
 
+import java.util.Locale;
+
+import com.liferay.portal.kernel.language.LanguageUtil;
 import com.liferay.portal.kernel.util.PropsKeys;
 import com.liferay.portal.kernel.util.PropsUtil;
 import com.liferay.portal.kernel.util.StringPool;
@@ -31,26 +34,52 @@ public class DefaultScreenNameValidator implements ScreenNameValidator {
 
 	@Override
 	public boolean validate(long companyId, String screenName) {
-		if (Validator.isEmailAddress(screenName) ||
-			StringUtil.equalsIgnoreCase(screenName, CYRUS) ||
-			StringUtil.equalsIgnoreCase(screenName, POSTFIX) ||
-			hasInvalidChars(screenName)) {
+		if (Validator.isEmailAddress(screenName)) {
+
+			_description = "the-screen-name-cannot-be-an-email-address";
 
 			return false;
 		}
-		else {
-			return true;
+		else if (StringUtil.equalsIgnoreCase(screenName, CYRUS) ||
+			StringUtil.equalsIgnoreCase(screenName, POSTFIX)) {
+
+			_arguments = "CYRUS, POSTFIX";
+			_description = "the-screen-name-cannot-be-a-reserved-word";
+
+			return false;
 		}
+
+		return !hasInvalidChars(screenName);
+	}
+
+	@Override
+	public String getDescription(Locale locale) {
+		if (_arguments != null) {
+			return LanguageUtil.format(locale, _description, _arguments, false);
+		}
+
+		return LanguageUtil.get(locale, _description);
 	}
 
 	private boolean hasInvalidChars(String screenName) {
-		String specialChars = PropsUtil.get(PropsKeys.USERS_SCREEN_NAME_SPECIAL_CHARACTERS);
+		String specialChars = PropsUtil.get(
+			PropsKeys.USERS_SCREEN_NAME_SPECIAL_CHARACTERS);
 
 		specialChars.replaceAll(StringPool.SLASH, StringPool.BLANK);
 
 		String validChars = "[A-Za-z0-9" + specialChars + "]+";
 
-		return !screenName.matches(validChars);
+		if (!screenName.matches(validChars)) {
+			_arguments = specialChars;
+			_description = "please-enter-a-valid-alphanumeric-screen-name";
+
+			return true;
+		}
+
+		return false;
 	}
+
+	private String _arguments;
+	private String _description;
 
 }
