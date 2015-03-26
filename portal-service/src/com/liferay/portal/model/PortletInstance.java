ea06ee705f5f39da2753bfa404b697b51eb47222
Jorge Ferrer <jorge.ferrer@liferay.com>
2015-03-26 16:23:40 +0100

    LPS-53947 Introduce new Factory Method to encapsulate the creation of a new portlet instance

--- portal-service/src/com/liferay/portal/model/PortletInstance.java
+++ portal-service/src/com/liferay/portal/model/PortletInstance.java
@@ -16,6 +16,7 @@ package com.liferay.portal.model;
 
 import com.liferay.portal.kernel.util.GetterUtil;
 import com.liferay.portal.kernel.util.StringBundler;
+import com.liferay.portal.kernel.util.StringUtil;
 import com.liferay.portal.kernel.util.Validator;
 
 /**
@@ -31,6 +32,10 @@ public class PortletInstance {
 	public static final int PORTLET_INSTANCE_ID_MAX_LENGTH =
 		ModelHintsUtil.getMaxLength(Portlet.class.getName(), "portletId") -
 			_INSTANCE_SEPARATOR.length() + _USER_SEPARATOR.length() + 39;
+
+	public static PortletInstance newInstanceFor(String rootPortletId) {
+		return new PortletInstance(rootPortletId, StringUtil.randomString(12));
+	}
 
 	public PortletInstance(String portletInstanceId) {
 		this(
