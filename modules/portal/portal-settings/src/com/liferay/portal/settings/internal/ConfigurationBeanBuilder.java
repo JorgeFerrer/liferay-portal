6c68e17802d98f808c1d49c49e81c9289db9be3d
Jorge Ferrer <jorge.ferrer@liferay.com>
2015-04-08 16:20:42 +0200

    LPS-54752 Reorganize implementations of Settings interface into their own package inside portal-service. This is to make the API cleaner and avoid mixing so many classes into one only package.

--- modules/portal/portal-settings/src/com/liferay/portal/settings/internal/ConfigurationBeanBuilder.java
+++ modules/portal/portal-settings/src/com/liferay/portal/settings/internal/ConfigurationBeanBuilder.java
@@ -12,7 +12,7 @@
  * details.
  */
 
-package com.liferay.portal.settings.impl;
+package com.liferay.portal.settings.internal;
 
 import aQute.bnd.annotation.metatype.Configurable;
 
--- modules/portal/portal-settings/src/com/liferay/portal/settings/internal/SettingsProviderBuilder.java
+++ modules/portal/portal-settings/src/com/liferay/portal/settings/internal/SettingsProviderBuilder.java
@@ -12,7 +12,7 @@
  * details.
  */
 
-package com.liferay.portal.settings.impl;
+package com.liferay.portal.settings.internal;
 
 /**
  * @author Iván Zaera
