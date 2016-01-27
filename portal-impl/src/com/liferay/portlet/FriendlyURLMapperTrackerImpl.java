653fa640a5b3df738067ea685e3dc4c52b86ab7a
Brian Chan <brian.chan@liferay.com>
2016-01-27 15:40:54 -0800

    LPS-44467 Is this right?

--- portal-impl/src/com/liferay/portlet/FriendlyURLMapperTrackerImpl.java
+++ portal-impl/src/com/liferay/portlet/FriendlyURLMapperTrackerImpl.java
@@ -129,7 +129,7 @@ public class FriendlyURLMapperTrackerImpl implements FriendlyURLMapperTracker {
 						_portlet.getFriendlyURLMapping());
 				}
 
-				friendlyURLMapper.setPortletId(_portlet.getPortletId());
+				friendlyURLMapper.setPortletName(_portlet.getPortletId());
 				friendlyURLMapper.setPortletInstanceable(
 					_portlet.isInstanceable());
 
