d2d767450b657668226cdb166c902df6f87d0c34
drewbrokke <drew.brokke@liferay.com>
2016-02-09 12:55:12 -0800

    LPS-56042 builds contacts service

--- modules/apps/contacts/contacts-service/src/main/resources/META-INF/sql/indexes.sql
+++ modules/apps/contacts/contacts-service/src/main/resources/META-INF/sql/indexes.sql
@@ -1,1 +1,3 @@
 create index IX_C257DE32 on Contacts_Entry (userId, emailAddress[$COLUMN_LENGTH:75$]);
+
+create index IX_89974FE on Entry (userId, emailAddress[$COLUMN_LENGTH:75$]);
--- modules/apps/contacts/contacts-service/src/main/resources/META-INF/sql/tables.sql
+++ modules/apps/contacts/contacts-service/src/main/resources/META-INF/sql/tables.sql
@@ -10,3 +10,16 @@ create table Contacts_Entry (
 	emailAddress VARCHAR(75) null,
 	comments STRING null
 );
+
+create table Entry (
+	entryId LONG not null primary key,
+	groupId LONG,
+	companyId LONG,
+	userId LONG,
+	userName VARCHAR(75) null,
+	createDate DATE null,
+	modifiedDate DATE null,
+	fullName VARCHAR(75) null,
+	emailAddress VARCHAR(75) null,
+	comments VARCHAR(75) null
+);
