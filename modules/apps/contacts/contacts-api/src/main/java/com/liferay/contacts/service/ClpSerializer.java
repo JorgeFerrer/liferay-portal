5ca607d55b06013b9f97ae129ce4f39f571faaf1
drewbrokke <drew.brokke@liferay.com>
2016-02-19 13:12:50 -0800

    LPS-63475 contacts-api - SF - ClpSerializer

--- modules/apps/contacts/contacts-api/src/main/java/com/liferay/contacts/service/ClpSerializer.java
+++ modules/apps/contacts/contacts-api/src/main/java/com/liferay/contacts/service/ClpSerializer.java
@@ -17,7 +17,6 @@ package com.liferay.contacts.service;
 import aQute.bnd.annotation.ProviderType;
 
 import com.liferay.contacts.model.EntryClp;
-
 import com.liferay.portal.kernel.io.unsync.UnsyncByteArrayInputStream;
 import com.liferay.portal.kernel.io.unsync.UnsyncByteArrayOutputStream;
 import com.liferay.portal.kernel.log.Log;
@@ -40,6 +39,7 @@ import java.util.List;
  */
 @ProviderType
 public class ClpSerializer {
+
 	public static String getServletContextName() {
 		if (Validator.isNotNull(_servletContextName)) {
 			return _servletContextName;
@@ -54,13 +54,14 @@ public class ClpSerializer {
 				ClassLoader classLoader = ClpSerializer.class.getClassLoader();
 
 				Class<?> portletPropsClass = classLoader.loadClass(
-						"com.liferay.util.portlet.PortletProps");
+					"com.liferay.util.portlet.PortletProps");
 
-				Method getMethod = portletPropsClass.getMethod("get",
-						new Class<?>[] { String.class });
+				Method getMethod = portletPropsClass.getMethod(
+					"get", new Class<?>[] {String.class});
 
-				String portletPropsServletContextName = (String)getMethod.invoke(null,
-						"contacts-portlet-deployment-context");
+				String portletPropsServletContextName =
+					(String)getMethod.invoke(
+						null, "contacts-portlet-deployment-context");
 
 				if (Validator.isNotNull(portletPropsServletContextName)) {
 					_servletContextName = portletPropsServletContextName;
@@ -69,14 +70,15 @@ public class ClpSerializer {
 			catch (Throwable t) {
 				if (_log.isInfoEnabled()) {
 					_log.info(
-						"Unable to locate deployment context from portlet properties");
+						"Unable to locate deployment context from portlet" +
+							"properties");
 				}
 			}
 
 			if (Validator.isNull(_servletContextName)) {
 				try {
 					String propsUtilServletContextName = PropsUtil.get(
-							"contacts-portlet-deployment-context");
+						"contacts-portlet-deployment-context");
 
 					if (Validator.isNotNull(propsUtilServletContextName)) {
 						_servletContextName = propsUtilServletContextName;
@@ -85,7 +87,8 @@ public class ClpSerializer {
 				catch (Throwable t) {
 					if (_log.isInfoEnabled()) {
 						_log.info(
-							"Unable to locate deployment context from portal properties");
+							"Unable to locate deployment context from portal" +
+								"properties");
 					}
 				}
 			}
@@ -111,7 +114,7 @@ public class ClpSerializer {
 	}
 
 	public static Object translateInput(List<Object> oldList) {
-		List<Object> newList = new ArrayList<Object>(oldList.size());
+		List<Object> newList = new ArrayList<>(oldList.size());
 
 		for (int i = 0; i < oldList.size(); i++) {
 			Object curObj = oldList.get(i);
@@ -120,16 +123,6 @@ public class ClpSerializer {
 		}
 
 		return newList;
-	}
-
-	public static Object translateInputEntry(BaseModel<?> oldModel) {
-		EntryClp oldClpModel = (EntryClp)oldModel;
-
-		BaseModel<?> newModel = oldClpModel.getEntryRemoteModel();
-
-		newModel.setModelAttributes(oldClpModel.getModelAttributes());
-
-		return newModel;
 	}
 
 	public static Object translateInput(Object obj) {
@@ -144,13 +137,24 @@ public class ClpSerializer {
 		}
 	}
 
+	public static Object translateInputEntry(BaseModel<?> oldModel) {
+		EntryClp oldClpModel = (EntryClp)oldModel;
+
+		BaseModel<?> newModel = oldClpModel.getEntryRemoteModel();
+
+		newModel.setModelAttributes(oldClpModel.getModelAttributes());
+
+		return newModel;
+	}
+
 	public static Object translateOutput(BaseModel<?> oldModel) {
 		Class<?> oldModelClass = oldModel.getClass();
 
 		String oldModelClassName = oldModelClass.getName();
 
 		if (oldModelClassName.equals(
-					"com.liferay.contacts.model.impl.EntryImpl")) {
+				"com.liferay.contacts.model.impl.EntryImpl")) {
+
 			return translateOutputEntry(oldModel);
 		}
 		else if (oldModelClassName.endsWith("Clp")) {
@@ -158,24 +162,27 @@ public class ClpSerializer {
 				ClassLoader classLoader = ClpSerializer.class.getClassLoader();
 
 				Method getClpSerializerClassMethod = oldModelClass.getMethod(
-						"getClpSerializerClass");
+					"getClpSerializerClass");
 
-				Class<?> oldClpSerializerClass = (Class<?>)getClpSerializerClassMethod.invoke(oldModel);
+				Class<?> oldClpSerializerClass =
+					(Class<?>)getClpSerializerClassMethod.invoke(oldModel);
 
-				Class<?> newClpSerializerClass = classLoader.loadClass(oldClpSerializerClass.getName());
+				Class<?> newClpSerializerClass = classLoader.loadClass(
+					oldClpSerializerClass.getName());
 
-				Method translateOutputMethod = newClpSerializerClass.getMethod("translateOutput",
-						BaseModel.class);
+				Method translateOutputMethod = newClpSerializerClass.getMethod(
+					"translateOutput", BaseModel.class);
 
 				Class<?> oldModelModelClass = oldModel.getModelClass();
 
-				Method getRemoteModelMethod = oldModelClass.getMethod("get" +
-						oldModelModelClass.getSimpleName() + "RemoteModel");
+				Method getRemoteModelMethod = oldModelClass.getMethod(
+					"get" + oldModelModelClass.getSimpleName() + "RemoteModel");
 
 				Object oldRemoteModel = getRemoteModelMethod.invoke(oldModel);
 
-				BaseModel<?> newModel = (BaseModel<?>)translateOutputMethod.invoke(null,
-						oldRemoteModel);
+				BaseModel<?> newModel =
+					(BaseModel<?>)translateOutputMethod.invoke(
+						null, oldRemoteModel);
 
 				return newModel;
 			}
@@ -190,7 +197,7 @@ public class ClpSerializer {
 	}
 
 	public static Object translateOutput(List<Object> oldList) {
-		List<Object> newList = new ArrayList<Object>(oldList.size());
+		List<Object> newList = new ArrayList<>(oldList.size());
 
 		for (int i = 0; i < oldList.size(); i++) {
 			Object curObj = oldList.get(i);
@@ -213,26 +220,42 @@ public class ClpSerializer {
 		}
 	}
 
+	public static Object translateOutputEntry(BaseModel<?> oldModel) {
+		EntryClp newModel = new EntryClp();
+
+		newModel.setModelAttributes(oldModel.getModelAttributes());
+
+		newModel.setEntryRemoteModel(oldModel);
+
+		return newModel;
+	}
+
 	public static Throwable translateThrowable(Throwable throwable) {
 		if (_useReflectionToTranslateThrowable) {
 			try {
-				UnsyncByteArrayOutputStream unsyncByteArrayOutputStream = new UnsyncByteArrayOutputStream();
-				ObjectOutputStream objectOutputStream = new ObjectOutputStream(unsyncByteArrayOutputStream);
+				UnsyncByteArrayOutputStream unsyncByteArrayOutputStream =
+					new UnsyncByteArrayOutputStream();
+				ObjectOutputStream objectOutputStream = new ObjectOutputStream(
+					unsyncByteArrayOutputStream);
 
 				objectOutputStream.writeObject(throwable);
 
 				objectOutputStream.flush();
 				objectOutputStream.close();
 
-				UnsyncByteArrayInputStream unsyncByteArrayInputStream = new UnsyncByteArrayInputStream(unsyncByteArrayOutputStream.unsafeGetByteArray(),
-						0, unsyncByteArrayOutputStream.size());
+				UnsyncByteArrayInputStream unsyncByteArrayInputStream =
+					new UnsyncByteArrayInputStream(
+						unsyncByteArrayOutputStream.unsafeGetByteArray(), 0,
+						unsyncByteArrayOutputStream.size());
 
 				Thread currentThread = Thread.currentThread();
 
-				ClassLoader contextClassLoader = currentThread.getContextClassLoader();
+				ClassLoader contextClassLoader =
+					currentThread.getContextClassLoader();
 
-				ObjectInputStream objectInputStream = new ClassLoaderObjectInputStream(unsyncByteArrayInputStream,
-						contextClassLoader);
+				ObjectInputStream objectInputStream =
+					new ClassLoaderObjectInputStream(
+						unsyncByteArrayInputStream, contextClassLoader);
 
 				throwable = (Throwable)objectInputStream.readObject();
 
@@ -266,49 +289,51 @@ public class ClpSerializer {
 		String className = clazz.getName();
 
 		if (className.equals(
-					"com.liferay.contacts.exception.DuplicateEntryEmailAddressException")) {
-			return new com.liferay.contacts.exception.DuplicateEntryEmailAddressException(throwable.getMessage(),
-				throwable.getCause());
+				"com.liferay.contacts.exception." +
+					"DuplicateEntryEmailAddressException")) {
+
+			return new com.liferay.contacts.exception.
+				DuplicateEntryEmailAddressException(
+					throwable.getMessage(), throwable.getCause());
 		}
 
 		if (className.equals(
-					"com.liferay.contacts.exception.EntryEmailAddressException")) {
-			return new com.liferay.contacts.exception.EntryEmailAddressException(throwable.getMessage(),
-				throwable.getCause());
+				"com.liferay.contacts.exception.EntryEmailAddressException")) {
+
+			return new com.liferay.contacts.exception.
+				EntryEmailAddressException(
+					throwable.getMessage(), throwable.getCause());
 		}
 
 		if (className.equals(
-					"com.liferay.contacts.exception.EntryFullNameException")) {
-			return new com.liferay.contacts.exception.EntryFullNameException(throwable.getMessage(),
-				throwable.getCause());
+				"com.liferay.contacts.exception.EntryFullNameException")) {
+
+			return new com.liferay.contacts.exception.EntryFullNameException(
+				throwable.getMessage(), throwable.getCause());
 		}
 
 		if (className.equals(
-					"com.liferay.contacts.exception.RequiredEntryEmailAddressException")) {
-			return new com.liferay.contacts.exception.RequiredEntryEmailAddressException(throwable.getMessage(),
-				throwable.getCause());
+				"com.liferay.contacts.exception." +
+					"RequiredEntryEmailAddressException")) {
+
+			return new com.liferay.contacts.exception.
+				RequiredEntryEmailAddressException(
+					throwable.getMessage(), throwable.getCause());
 		}
 
 		if (className.equals(
-					"com.liferay.contacts.exception.NoSuchEntryException")) {
-			return new com.liferay.contacts.exception.NoSuchEntryException(throwable.getMessage(),
-				throwable.getCause());
+				"com.liferay.contacts.exception.NoSuchEntryException")) {
+
+			return new com.liferay.contacts.exception.NoSuchEntryException(
+				throwable.getMessage(), throwable.getCause());
 		}
 
 		return throwable;
 	}
 
-	public static Object translateOutputEntry(BaseModel<?> oldModel) {
-		EntryClp newModel = new EntryClp();
+	private static final Log _log = LogFactoryUtil.getLog(ClpSerializer.class);
 
-		newModel.setModelAttributes(oldModel.getModelAttributes());
-
-		newModel.setEntryRemoteModel(oldModel);
-
-		return newModel;
-	}
-
-	private static Log _log = LogFactoryUtil.getLog(ClpSerializer.class);
 	private static String _servletContextName;
 	private static boolean _useReflectionToTranslateThrowable = true;
+
 }
