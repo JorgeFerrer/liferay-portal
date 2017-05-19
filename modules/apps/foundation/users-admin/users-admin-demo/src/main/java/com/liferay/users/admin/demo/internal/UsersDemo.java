787ad57b991ecb2998147a46b92f6cdbd7e249af
Pei-Jung Lan <pei-jung.lan@liferay.com>
2017-05-19 11:55:14 -0700

    LPS-72477 Update users demo

--- modules/apps/foundation/users-admin/users-admin-demo/src/main/java/com/liferay/users/admin/demo/internal/UsersDemo.java
+++ modules/apps/foundation/users-admin/users-admin-demo/src/main/java/com/liferay/users/admin/demo/internal/UsersDemo.java
@@ -19,8 +19,12 @@ import com.liferay.portal.instance.lifecycle.PortalInstanceLifecycleListener;
 import com.liferay.portal.kernel.exception.PortalException;
 import com.liferay.portal.kernel.model.Company;
 import com.liferay.portal.kernel.model.Group;
+import com.liferay.portal.kernel.model.Role;
+import com.liferay.portal.kernel.util.StringUtil;
+import com.liferay.roles.admin.demo.data.creator.RoleDemoDataCreator;
 import com.liferay.site.demo.data.creator.SiteDemoDataCreator;
 import com.liferay.users.admin.demo.data.creator.SiteAdminUserDemoDataCreator;
+import com.liferay.users.admin.demo.data.creator.SiteMemberUserDemoDataCreator;
 import com.liferay.users.admin.demo.data.creator.VirtualInstanceAdminUserDemoDataCreator;
 
 import org.osgi.service.component.annotations.Component;
@@ -35,21 +39,49 @@ public class UsersDemo extends BasePortalInstanceLifecycleListener {
 
 	@Override
 	public void portalInstanceRegistered(Company company) throws Exception {
+		long companyId = company.getCompanyId();
+
 		Group acmeCorpGroup = _siteDemoDataCreator.create(
-			company.getCompanyId(), "Acme’s Corporation");
+			companyId, "Acme’s Corporation");
 
 		_siteAdminUserDemoDataCreator.create(
 			acmeCorpGroup.getGroupId(), "helen@liferay.com");
 
 		_virtualInstanceAdminUserDemoDataCreator.create(
-			company.getCompanyId(), "bruno@liferay.com");
+			companyId, "bruno@liferay.com");
+
+		String webContentAuthorXml = StringUtil.read(
+			UsersDemo.class, "dependencies/web-content-author.xml");
+
+		Role webContentAuthor = _siteRoleDemoDataCreator.create(
+			companyId, "Web Content Author", webContentAuthorXml);
+
+		_siteMemberUserDemoDataCreator.create(
+			acmeCorpGroup.getGroupId(), "joe@liferay.com",
+			new long[] {webContentAuthor.getRoleId()});
+
+		Group petLoversGroup = _siteDemoDataCreator.create(
+			companyId, "Pet Lovers");
+
+		String forumModeratorXml = StringUtil.read(
+			UsersDemo.class, "dependencies/forum-moderator.xml");
+
+		Role forumModerator = _siteRoleDemoDataCreator.create(
+			companyId, "Forum Moderator", forumModeratorXml);
+
+		_siteMemberUserDemoDataCreator.create(
+			petLoversGroup.getGroupId(), "maria@liferay.com",
+			new long[] {forumModerator.getRoleId()});
 	}
 
 	@Deactivate
 	protected void deactivate() throws PortalException {
-		_virtualInstanceAdminUserDemoDataCreator.delete();
 		_siteAdminUserDemoDataCreator.delete();
+		_siteMemberUserDemoDataCreator.delete();
+		_virtualInstanceAdminUserDemoDataCreator.delete();
+
 		_siteDemoDataCreator.delete();
+		_siteRoleDemoDataCreator.delete();
 	}
 
 	@Reference
@@ -57,6 +89,12 @@ public class UsersDemo extends BasePortalInstanceLifecycleListener {
 
 	@Reference
 	private SiteDemoDataCreator _siteDemoDataCreator;
+
+	@Reference
+	private SiteMemberUserDemoDataCreator _siteMemberUserDemoDataCreator;
+
+	@Reference(target = "(role.type=site)")
+	private RoleDemoDataCreator _siteRoleDemoDataCreator;
 
 	@Reference
 	private VirtualInstanceAdminUserDemoDataCreator
