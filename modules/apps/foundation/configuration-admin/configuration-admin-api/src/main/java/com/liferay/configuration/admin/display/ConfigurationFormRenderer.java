ae930cd3ce24fdcd8ba49a917f6aa7579b8f6615
Jorge Ferrer <jorge.ferrer@liferay.com>
2018-05-08 22:24:14 +0200

    LPS-80341 Extend ConfigurationFormRenderer to provide custom reads of the params so that the inputs can have any desired name
    Note 1: It was not possible to move the default behavior to a DefaultConfigurationFormRenderer because DDM requires too many parameters such as ConfigurationModel which is and should remain internal. Because of this I've had to rely on "null" as a special value to trigger using DDM to read the form values. It's not super pretty, but I couldn't think of a better option.
    
    Note 2: I've added the conversion from Map to Dictionary to avoid propagating the usage of the legacy Dictionary class to the interface. We are internally using due to OSGi's APIs, be we should not expose this undesired aspect propagate further. The cost of the conversion is small since this is only done on configuration save.

--- modules/apps/foundation/configuration-admin/configuration-admin-api/src/main/java/com/liferay/configuration/admin/display/ConfigurationFormRenderer.java
+++ modules/apps/foundation/configuration-admin/configuration-admin-api/src/main/java/com/liferay/configuration/admin/display/ConfigurationFormRenderer.java
@@ -15,6 +15,7 @@
 package com.liferay.configuration.admin.display;
 
 import java.io.IOException;
+import java.util.Map;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -25,6 +26,8 @@ import javax.servlet.http.HttpServletResponse;
 public interface ConfigurationFormRenderer {
 
 	public String getPid();
+
+	public Map<String, Object> getRequestParameters(HttpServletRequest request);
 
 	public void render(HttpServletRequest request, HttpServletResponse response)
 		throws IOException;
