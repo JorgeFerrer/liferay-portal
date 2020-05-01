1f3e5efbab22a39c6705c48f51a8b880505ec7d6
Jorge Ferrer <jorge.ferrer@liferay.com>
2020-05-01 22:20:47 +0200

    LPS-112372 Provide helper providers and readers to facilitate including expando attributes through in InfoItemFieldDescriptor implementations

---
+++ modules/apps/expando/expando-web/src/main/java/com/liferay/expando/web/internal/info/fields/provider/ExpandoInfoItemFieldsProviderImpl.java
@@ -0,0 +1,72 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.expando.web.internal.info.fields.provider;
+
+import com.liferay.expando.info.item.fields.provider.ExpandoInfoItemFieldsProvider;
+import com.liferay.expando.info.item.fields.reader.ExpandoInfoItemFieldReader;
+import com.liferay.expando.kernel.model.ExpandoBridge;
+import com.liferay.expando.kernel.util.ExpandoBridgeFactoryUtil;
+import com.liferay.info.item.fields.InfoItemField;
+import com.liferay.portal.kernel.security.auth.CompanyThreadLocal;
+
+import java.util.ArrayList;
+import java.util.Enumeration;
+import java.util.List;
+
+import org.osgi.service.component.annotations.Component;
+
+/**
+ * @author Jürgen Kappler
+ * @author Jorge Ferrer
+ */
+@Component(immediate = true, service = ExpandoInfoItemFieldsProvider.class)
+public class ExpandoInfoItemFieldsProviderImpl
+	implements ExpandoInfoItemFieldsProvider {
+
+	@Override
+	public List<InfoItemField> getFields(String itemClassName) {
+		List<InfoItemField> infoItemFields = new ArrayList<>();
+
+		for (ExpandoInfoItemFieldReader expandoInfoItemFieldReader :
+				_getExpandoFieldReaders(itemClassName)) {
+
+			infoItemFields.add(expandoInfoItemFieldReader.getField());
+		}
+
+		return infoItemFields;
+	}
+
+	private List<ExpandoInfoItemFieldReader> _getExpandoFieldReaders(
+		String itemClassName) {
+
+		List<ExpandoInfoItemFieldReader> expandoInfoItemFieldReaders =
+			new ArrayList<>();
+
+		ExpandoBridge expandoBridge = ExpandoBridgeFactoryUtil.getExpandoBridge(
+			CompanyThreadLocal.getCompanyId(), itemClassName, 0L);
+
+		Enumeration<String> attributeNames = expandoBridge.getAttributeNames();
+
+		while (attributeNames.hasMoreElements()) {
+			String attributeName = attributeNames.nextElement();
+
+			expandoInfoItemFieldReaders.add(
+				new ExpandoInfoItemFieldReader(attributeName, expandoBridge));
+		}
+
+		return expandoInfoItemFieldReaders;
+	}
+
+}
