e3bb0f46f311d61436edfe045c0ca7ca8da69859
Jorge Ferrer <jorge.ferrer@liferay.com>
2020-05-09 13:49:51 +0200

    LPS-112372 Implement getInfoFieldSet(T t) and getInfoFieldSet(long classPK) in the InfoDisplayContributor wrapper

--- modules/apps/info/info-impl/src/main/java/com/liferay/info/internal/display/contributor/InfoDisplayContributorInfoItemDescriptorWrapper.java
+++ modules/apps/info/info-impl/src/main/java/com/liferay/info/internal/display/contributor/InfoDisplayContributorInfoItemDescriptorWrapper.java
@@ -24,6 +24,7 @@ import com.liferay.info.fields.type.InfoFieldType;
 import com.liferay.info.fields.type.TextInfoFieldType;
 import com.liferay.info.fields.type.URLInfoFieldType;
 import com.liferay.info.internal.util.GenericsUtil;
+import com.liferay.info.item.NoSuchSubtypeException;
 import com.liferay.info.item.descriptor.InfoItemDescriptor;
 import com.liferay.info.localized.LocalizedValue;
 import com.liferay.portal.kernel.exception.PortalException;
@@ -48,42 +49,82 @@ public class InfoDisplayContributorInfoItemDescriptorWrapper
 	public InfoFieldSet getInfoFieldSet() {
 		Locale locale = LocaleThreadLocal.getThemeDisplayLocale();
 
-		LocalizedValue fieldSetLabel = LocalizedValue.builder(
-		).addValue(
-			locale, "default"
-		).build();
-
-		InfoFieldSet infoFieldSet = new InfoFieldSet(fieldSetLabel, "fields");
-
 		try {
 			Set<InfoDisplayField> infoDisplayFields =
 				_infoDisplayContributor.getInfoDisplayFields(0, locale);
 
-			for (InfoDisplayField infoDisplayField : infoDisplayFields) {
-				InfoFieldType type = _getInfoFieldTypeType(
-					infoDisplayField.getType());
-
-				LocalizedValue fieldLabel = LocalizedValue.builder(
-				).addValue(
-					locale, infoDisplayField.getLabel()
-				).build();
-
-				InfoField infoField = new InfoField(
-					fieldLabel, infoDisplayField.getKey(), type);
-
-				infoFieldSet.add(infoField);
-			}
+			return _convertToInfoFieldSet(infoDisplayFields);
 		}
 		catch (PortalException portalException) {
 			throw new RuntimeException(portalException);
 		}
+	}
 
-		return infoFieldSet;
+	@Override
+	public InfoFieldSet getInfoFieldSet(long classTypeId)
+		throws NoSuchSubtypeException {
+
+		Locale locale = LocaleThreadLocal.getThemeDisplayLocale();
+
+		try {
+			return _convertToInfoFieldSet(
+				_infoDisplayContributor.getInfoDisplayFields(
+					classTypeId, locale));
+		}
+		catch (PortalException portalException) {
+			throw new RuntimeException(
+				"Error retrieving fields for classTypeId: " + classTypeId,
+				portalException);
+		}
+	}
+
+	@Override
+	public InfoFieldSet getInfoFieldSet(Object infoItemObject) {
+		Locale locale = LocaleThreadLocal.getThemeDisplayLocale();
+
+		try {
+			return _convertToInfoFieldSet(
+				_infoDisplayContributor.getInfoDisplayFields(
+					infoItemObject, locale));
+		}
+		catch (PortalException portalException) {
+			throw new RuntimeException(portalException);
+		}
 	}
 
 	@Override
 	public String getItemClassName() {
 		return GenericsUtil.getItemClassName(_infoDisplayContributor);
+	}
+
+	private InfoFieldSet _convertToInfoFieldSet(
+		Set<InfoDisplayField> infoDisplayFields) {
+
+		Locale locale = LocaleThreadLocal.getThemeDisplayLocale();
+
+		LocalizedValue fieldSetLabel = LocalizedValue.builder(
+		).addValue(
+			locale, "fields"
+		).build();
+
+		InfoFieldSet infoFieldSet = new InfoFieldSet(fieldSetLabel, "fields");
+
+		for (InfoDisplayField infoDisplayField : infoDisplayFields) {
+			InfoFieldType type = _getInfoFieldTypeType(
+				infoDisplayField.getType());
+
+			LocalizedValue fieldLabel = LocalizedValue.builder(
+			).addValue(
+				locale, infoDisplayField.getLabel()
+			).build();
+
+			InfoField infoField = new InfoField(
+				fieldLabel, infoDisplayField.getKey(), type);
+
+			infoFieldSet.add(infoField);
+		}
+
+		return infoFieldSet;
 	}
 
 	private InfoFieldType _getInfoFieldTypeType(String infoDisplayFieldType) {
