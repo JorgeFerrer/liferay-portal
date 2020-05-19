cc7b47af02dfe60aebfba42fa4a7ac52e41a2b19
Jorge Ferrer <jorge.ferrer@liferay.com>
2020-05-19 22:48:26 +0200

    LPS-112372 Do a final attempt at finding a String as a key in the kernel's language bundle

--- modules/apps/info/info-api/src/main/java/com/liferay/info/localized/InfoLocalizedValue.java
+++ modules/apps/info/info-api/src/main/java/com/liferay/info/localized/InfoLocalizedValue.java
@@ -16,6 +16,7 @@ package com.liferay.info.localized;
 
 import com.liferay.info.localized.bundle.ResourceBundleInfoLocalizedValue;
 import com.liferay.petra.lang.HashUtil;
+import com.liferay.portal.kernel.language.LanguageUtil;
 import com.liferay.portal.kernel.util.LocaleUtil;
 
 import java.util.HashMap;
@@ -139,6 +140,10 @@ public interface InfoLocalizedValue<T> {
 
 			if (value == null) {
 				value = _builder._values.get(getDefaultLocale());
+			}
+
+			if (value instanceof String) {
+				value = (T)LanguageUtil.get(locale, (String)value);
 			}
 
 			return value;
