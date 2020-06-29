970534feb2d7e0d7cb83a8ac64e955896693c002
Pavel Savinov <pavel.savinov@liferay.com>
2020-06-29 15:08:41 +0200

    LPS-116031 Create categorization info field set with available vocabularies as info fields

--- modules/apps/asset/asset-info-impl/src/main/java/com/liferay/asset/info/internal/item/provider/AssetEntryInfoItemFieldSetProviderImpl.java
+++ modules/apps/asset/asset-info-impl/src/main/java/com/liferay/asset/info/internal/item/provider/AssetEntryInfoItemFieldSetProviderImpl.java
@@ -21,6 +21,8 @@ import com.liferay.asset.kernel.model.AssetCategory;
 import com.liferay.asset.kernel.model.AssetEntry;
 import com.liferay.asset.kernel.model.AssetRendererFactory;
 import com.liferay.asset.kernel.model.AssetTag;
+import com.liferay.asset.kernel.model.AssetVocabulary;
+import com.liferay.asset.kernel.service.AssetVocabularyLocalService;
 import com.liferay.info.exception.NoSuchInfoItemException;
 import com.liferay.info.field.InfoField;
 import com.liferay.info.field.InfoFieldSet;
@@ -29,15 +31,19 @@ import com.liferay.info.field.type.TextInfoFieldType;
 import com.liferay.info.item.field.reader.InfoItemFieldReaderFieldSetProvider;
 import com.liferay.info.localized.InfoLocalizedValue;
 import com.liferay.petra.string.StringBundler;
+import com.liferay.petra.string.StringPool;
 import com.liferay.portal.kernel.exception.PortalException;
-import com.liferay.portal.kernel.util.Accessor;
 import com.liferay.portal.kernel.util.ListUtil;
 import com.liferay.portal.kernel.util.LocaleThreadLocal;
 import com.liferay.portal.kernel.util.Validator;
 
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Locale;
+import java.util.Set;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 import org.osgi.service.component.annotations.Component;
 import org.osgi.service.component.annotations.Reference;
@@ -48,6 +54,32 @@ import org.osgi.service.component.annotations.Reference;
 @Component(immediate = true, service = AssetEntryInfoItemFieldSetProvider.class)
 public class AssetEntryInfoItemFieldSetProviderImpl
 	implements AssetEntryInfoItemFieldSetProvider {
+
+	@Override
+	public InfoFieldSet getInfoFieldSet(AssetEntry assetEntry) {
+		InfoFieldSet infoFieldSet = getInfoFieldSet(AssetEntry.class.getName());
+
+		Locale locale = LocaleThreadLocal.getThemeDisplayLocale();
+		Set<AssetVocabulary> assetVocabularies = _getAssetVocabularies(
+			assetEntry);
+
+		InfoFieldSet categorizationInfoFieldSet = new InfoFieldSet(
+			InfoLocalizedValue.localize(getClass(), "categorization"),
+			"categorization");
+
+		for (AssetVocabulary assetVocabulary : assetVocabularies) {
+			categorizationInfoFieldSet.add(
+				new InfoField(
+					TextInfoFieldType.INSTANCE,
+					InfoLocalizedValue.localize(
+						getClass(), assetVocabulary.getTitle(locale)),
+					assetVocabulary.getName()));
+		}
+
+		infoFieldSet.add(categorizationInfoFieldSet);
+
+		return infoFieldSet;
+	}
 
 	@Override
 	public InfoFieldSet getInfoFieldSet(String itemClassName) {
@@ -73,7 +105,7 @@ public class AssetEntryInfoItemFieldSetProviderImpl
 
 		infoFieldValues.add(
 			new InfoFieldValue<>(
-				_categoriesInfoField, () -> _getCategoryNames(assetEntry)));
+				_categoriesInfoField, () -> _getCategoryNames(assetEntry, 0)));
 
 		infoFieldValues.add(
 			new InfoFieldValue<>(
@@ -115,36 +147,52 @@ public class AssetEntryInfoItemFieldSetProviderImpl
 		}
 	}
 
-	private String _getCategoryNames(AssetEntry assetEntry) {
+	private Set<AssetVocabulary> _getAssetVocabularies(AssetEntry assetEntry) {
+		List<AssetCategory> assetCategories = assetEntry.getCategories();
+		Set<AssetVocabulary> assetVocabularies = new HashSet<>();
+
+		for (AssetCategory assetCategory : assetCategories) {
+			AssetVocabulary assetVocabulary =
+				_assetVocabularyLocalService.fetchAssetVocabulary(
+					assetCategory.getVocabularyId());
+
+			assetVocabularies.add(assetVocabulary);
+		}
+
+		return assetVocabularies;
+	}
+
+	private String _getCategoryNames(AssetEntry assetEntry, long vocabularyId) {
 		Locale locale = LocaleThreadLocal.getThemeDisplayLocale();
 
-		return ListUtil.toString(
-			assetEntry.getCategories(),
-			new Accessor<AssetCategory, String>() {
+		List<AssetCategory> assetCategories = assetEntry.getCategories();
 
-				@Override
-				public String get(AssetCategory assetCategory) {
-					String title = assetCategory.getTitle(locale);
+		if (vocabularyId > 0) {
+			assetCategories = ListUtil.filter(
+				assetCategories,
+				assetCategory ->
+					assetCategory.getVocabularyId() == vocabularyId);
+		}
 
-					if (Validator.isNull(title)) {
-						return assetCategory.getName();
-					}
+		Stream<AssetCategory> stream = assetCategories.stream();
 
-					return title;
+		return stream.map(
+			assetCategory -> {
+				String title = assetCategory.getTitle(locale);
+
+				if (Validator.isNull(title)) {
+					return assetCategory.getName();
 				}
 
-				@Override
-				public Class<String> getAttributeClass() {
-					return String.class;
-				}
-
-				@Override
-				public Class<AssetCategory> getTypeClass() {
-					return AssetCategory.class;
-				}
-
-			});
+				return title;
+			}
+		).collect(
+			Collectors.joining(StringPool.COMMA_AND_SPACE)
+		);
 	}
+
+	@Reference
+	private AssetVocabularyLocalService _assetVocabularyLocalService;
 
 	private final InfoField _categoriesInfoField = new InfoField(
 		TextInfoFieldType.INSTANCE,
